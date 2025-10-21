5bb368cc69eb33ac6bff757180ca252907dcccb4
jasonrn36 <jason.rn36@gmail.com>
2025-10-21 14:17:39 -0400

    configurando as constantes da tarefa e as funções

---
+++ gulpfile.js
@@ -0,0 +1,8 @@
+// AREA DA CONFIGURAÇÃO DAS CONSTANTES E VARIÁVEIS
+const gulp = require ('gulp');
+const sass = require ('sass')(require('sass'));
+
+// AREA DAS FUNÇÕES DA TAREFA
+
+
+exports.default = testGulp;
