f47dd26da5ea7baba701ba40eb5365da4baa5eca
jasonrn36 <jason.rn36@gmail.com>
2025-10-23 12:21:10 -0400

    feito a troca das fuções de teste por gulp sass

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,22 @@
-function testGulp(cb) {
-    console.log("Olá, Mundo!");
-    cb();
+//  AREA DAS CONSTANTES
+const gulp = require('gulp');
+const sass = require('gulp-sass')(require('sass')); // ATENÇÃO O GULP-SASS E DEPOIS O SASS EXEMPLO (gulp-sass)  (sass)
+
+//AREA DAS TAREFAS OU FUNÇÕES
+//  ESTA TAREFA CONVERTE SCSS EM CSS
+function Styles() {
+    console.log("A tarefa Converteu o scss em css")
+    return gulp.src('./src/Estilos/*.scss').pipe(sass({outputStyle:'compressed'})).pipe(gulp.dest('./dist/css'))
 
 }
+// Tarefa para copiar JS
+function copiarJS() {
+    return gulp.src('src/js/**/*.js')
+    .pipe(gulp.dest('dist/js'));
+}
+
+
 // AREA DE EXECUÇÃO OU EXPORTAÇÃO DA TAREFA
-exports.default = testGulp;
+exports.default = Styles; //Aqui deve ser digitado o nome da função
+// Tarefa em série: primeiro Sass, depois JS
+exports.build = gulp.series(Styles, copiarJS);
---
+++ src/js/main.js
@@ -0,0 +1,2 @@
+console.log('\n✅Testando essa A tarefa converteu o scss em css\n');
+console.log('%cSucesso!', 'color: green; background-color: yellow; padding: 4px;');
