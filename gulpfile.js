21bead8b3cfb98297a2a70b805de556fa4d07824
jasonrn36 <jason.rn36@gmail.com>
2025-10-28 17:03:05 -0400

    trocado o imagemin po sharp e glob para compressão da imagem

--- gulpfile.js
+++ gulpfile.js
@@ -1,26 +1,52 @@
 //  AREA DAS CONSTANTES
 const gulp = require('gulp');
+const uglify = require('gulp-uglify');
+const rename = require('gulp-rename');
 const sass = require('gulp-sass')(require('sass')); // ATENÇÃO O GULP-SASS E DEPOIS O SASS EXEMPLO (gulp-sass) (sass)
-const imagemin = require('gulp-imagemin');
+const { exec } = require('child_process');
 
-
-//AREA DAS TAREFAS OU FUNÇÕES
+//  AREA DAS TAREFAS OU FUNÇÕES
 //  ESTA TAREFA CONVERTE SCSS EM CSS
 function Styles() {
     return gulp.src('./src/Estilos/*.scss').pipe(sass({outputStyle:'compressed'})).pipe(gulp.dest('./dist/css'))
 
 }
 
-function Imagens() {
-    return gulp.src('./src/Images/**/*.{jpg,jpeg,png,svg,gif,}')
-    .pipe(imagemin())
-    .pipe(gulp.dest('./dist/Images'));
+// Caminhos
+const paths = {
+scripts: {
+    src: './src/js/*.js',
+    dest: './dist/js'
+}
+};
 
+// Tarefa para minificar JS
+function compressJS() {
+return gulp.src(paths.scripts.src)
+    .pipe(uglify())
+    .pipe(rename({ suffix: '.min' }))
+    .pipe(gulp.dest(paths.scripts.dest));
 }
 
-    exports.default = (Styles,Imagens);
-    exports.watch = function(){
-    gulp.watch('./src/Estilos/*.scss', gulp.parallel(Styles))
-    gulp.watch('./src/Images/**/*.{jpg,jpeg,png,svg,gif,}', gulp.parallel(Imagens));
 
+//Tarefa para executar o compressor.js
+    gulp.task('compress-images', (done) => {
+    exec('node ./src/js/compressor.js', (err, stdout, stderr) => {
+        if (err) {
+        console.error(`Erro: ${stderr}`);
+        } else {
+        console.log(stdout);
+    }
+done(); })
+});
+
+
+    exports.default = gulp.series(Styles, 'compress-images',compressJS); // Usado para criar os arquivos apos cada modificação
+
+    exports.watch = function() { // Usado para assistir as modificações enquanto trabalha
+
+    gulp.watch('../src/Estilos/*.scss', gulp.series(Styles))
+    gulp.watch('./src/Images/*.scss', gulp.series('compress-images'));
+    gulp.watch('./src/js/*.js', gulp.series(compressJS));
+    
 }
---
+++ src/js/compressor.js
@@ -0,0 +1,43 @@
+const sharp = require('sharp');
+const glob = require('glob');
+const fs = require('fs-extra');
+const path = require('path');
+
+// Pastas do projeto "test"
+const pastaEntrada = 'src/Images';
+const pastaSaida = 'dist/Images';
+
+fs.ensureDirSync(pastaSaida);
+
+glob(`${pastaEntrada}/**/*.{jpg,jpeg,png,gif}`, (err, arquivos) => {
+if (err) {
+    console.error('Erro ao buscar imagens:', err);
+    return;
+}
+
+arquivos.forEach(async (arquivo) => {
+    const extensao = path.extname(arquivo).toLowerCase();
+    const nomeArquivo = path.basename(arquivo);
+    const destino = path.join(pastaSaida, nomeArquivo);
+
+    try {
+    if (extensao === '.jpg' || extensao === '.jpeg') {
+        await sharp(arquivo)
+        .jpeg({ quality: 70 })
+        .toFile(destino);
+    } else if (extensao === '.png') {
+        await sharp(arquivo)
+        .png({ compressionLevel: 20 })
+        .toFile(destino);
+    } else if (extensao === '.gif') {
+        await sharp(arquivo)
+        .gif({ effort: 1 })
+        .toFile(destino);
+    }
+
+    console.log(`Imagem comprimida: ${arquivo} → ${destino}`);
+    } catch (erro) {
+    console.error(`Erro ao processar ${arquivo}:`, erro.message);
+    }
+});
+});
