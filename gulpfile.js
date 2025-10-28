d63a8a81abdda83c9753a3aafae2b7d9547052b5
jasonrn36 <jason.rn36@gmail.com>
2025-10-28 10:32:27 -0400

    hospedando as imagens no vercel e usando elas via link

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,7 @@
 //  AREA DAS CONSTANTES
 const gulp = require('gulp');
 const sass = require('gulp-sass')(require('sass')); // ATENÇÃO O GULP-SASS E DEPOIS O SASS EXEMPLO (gulp-sass)  (sass)
-const imagemin = require('gulp-imagemin');
+
 
 //AREA DAS TAREFAS OU FUNÇÕES
 //  ESTA TAREFA CONVERTE SCSS EM CSS
@@ -10,16 +10,11 @@ function Styles() {
 
 }
 
-function MinificaImagem() {
-    return gulp.src('./src/Images/**/*')
-    .pipe(imagemin())
-    .pipe(gulp.dest('./dist/Images'))
 
-}
 
-exports.default = gulp.parallel(Styles, MinificaImagem);
+    exports.default = Styles; // Esta tarefa não deve ser apagada ela é a padrão
+        exports.watch = function(){
 
-    exports.watch = function(){
         gulp.watch('./src/Estilos/*.scss', gulp.parallel(Styles))
 
 }
