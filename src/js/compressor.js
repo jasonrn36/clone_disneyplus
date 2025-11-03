211deb4b3c41b3bb5736c7f93fafb52a1c0acae2
jsonrn36 <jason.rn36@gmail.com>
2025-11-03 15:18:37 -0400

    contertando para entregar o arquivo no caminho relativo

--- src/js/compressor.js
+++ src/js/compressor.js
@@ -3,7 +3,7 @@ const glob = require('glob');
 const fs = require('fs-extra');
 const path = require('path');
 
-// Pastas do projeto "test"
+
 const pastaEntrada = 'src/Images';
 const pastaSaida = 'dist/Images';
 
@@ -17,22 +17,18 @@ if (err) {
 
 arquivos.forEach(async (arquivo) => {
     const extensao = path.extname(arquivo).toLowerCase();
-    const nomeArquivo = path.basename(arquivo);
-    const destino = path.join(pastaSaida, nomeArquivo);
+    const caminhoRelativo = path.relative(pastaEntrada, arquivo);
+    const destino = path.join(pastaSaida, caminhoRelativo);
 
     try {
+      await fs.ensureDir(path.dirname(destino)); // Garante que a subpasta exista
+
     if (extensao === '.jpg' || extensao === '.jpeg') {
-        await sharp(arquivo)
-        .jpeg({ quality: 75 })
-        .toFile(destino);
+        await sharp(arquivo).jpeg({ quality: 75 }).toFile(destino);
     } else if (extensao === '.png') {
-        await sharp(arquivo)
-        .png({ compressionLevel: 20 })
-        .toFile(destino);
+        await sharp(arquivo).png({ compressionLevel: 9 }).toFile(destino);
     } else if (extensao === '.gif') {
-        await sharp(arquivo)
-        .gif({ effort: 3 })
-        .toFile(destino);
+        await sharp(arquivo).gif({ effort: 3 }).toFile(destino);
     }
 
     console.log(`Imagem comprimida: ${arquivo} → ${destino}`);
