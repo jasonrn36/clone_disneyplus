cefdbeb8a735fdf764fbbf0702367fa7ee0a1ebb
jsonrn36 <jason.rn36@gmail.com>
2025-11-05 16:24:06 -0400

    criando a função para esconder a borda do botão ativo

--- src/js/main.js
+++ src/js/main.js
@@ -6,21 +6,26 @@ document.addEventListener('DOMContentLoaded', function(){
         buttons[i].addEventListener('click', function(botao){
             const abaAlvo = botao.target.dataset.tabButton;
             const aba = document.querySelector(`[data-tab-id=${abaAlvo}]`);
-            escondeTodasAbas();
+
+            escondeTodasAbas();//chama a função para esconder todas as abas
             aba.classList.add('shows__list--is-active');
+
+            removeBotaoAtivo(); // chama a função para remover o botão ativo
+            botao.target.classList.add('shows__tabs__button--is-active');
 
         })
     }
 })
-
+//Area_de_remoção do botão ativo
 function removeBotaoAtivo() {
-    const buttons = document.querySelectorAll('[data-tab-id]');
+    const buttons = document.querySelectorAll('[data-tab-button]');
+
     for (let i = 0; i< buttons.length; i++) {
-        buttons[i].classList.remove('shows__list--is-active');
+        buttons[i].classList.remove('shows__tabs__button--is-active');
     }
 
 }
-
+//Area de ocultar todas as abas
 function escondeTodasAbas() {
     const tabsContainer = document.querySelectorAll('[data-tab-id]');
 
@@ -28,3 +33,5 @@ function escondeTodasAbas() {
         tabsContainer[i].classList.remove('shows__list--is-active');
     }
 }
+
+//Area de botão ativo
